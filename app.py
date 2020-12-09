28705c8e15e5ffec9c72e7822a3555cb4b5fe1f9
shwang0416 <72377329+shwang0416@users.noreply.github.com>
2020-12-10 00:46:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, render_template, session, jsonify
-from pymongo import MongoClient
+from datetime import timedelta
 
-# dddddd
+from bs4 import BeautifulSoup
+from flask import Flask, request, render_template, session, jsonify, redirect, url_for
+from pip._vendor import requests
+from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client.users
@@ -10,12 +12,15 @@ app = Flask(__name__)
 app.secret_key = 'asdfgh123'
 
 
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
+
+
 @app.route('/')
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -23,29 +28,114 @@ def register():
     if request.method == 'POST':
         id_receive = request.form['id_give']
         pw_receive = request.form['pw_give']
+        pw2_receive = request.form['pw2_give']
 
-        new_user = {'id': id_receive, 'pw': pw_receive}
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify({'result': 'success', 'msg': "회원가입 성공!"})
+        user = db.users.find_one({'id': id_receive})
+
+        if user is not None or pw_receive != pw2_receive:
+            return jsonify({'result': 'fail'})
+        else:
+            new_user = {'id': id_receive, 'pw': pw_receive}
+            # print(new_user)
+            db.users.insert_one(new_user)
+            return jsonify({'result': 'success'})
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    if request.method == "GET":
-        return render_template('login.html')
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    user = db.users.find_one({'id': id_receive})
+
+    if user is not None and pw_receive == user['pw']:
+        # 유저가 db에 있을 경우
+        session['user'] = id_receive
+        # print(session)
+        # print("확인")
+        return jsonify({'result': 'success'})
     else:
-        id_receive = request.form['id_give']
-        pw_receive = request.form['pw_give']
-        try:
-            user = db.users.find_one({'id': id_receive}, {'pw': pw_receive})
-            if user is not None:
-                session['logged_in'] = True
-                return jsonify({'result': 'success', 'msg': "로그인 상태!"})
-            else:
-                return jsonify({'result': 'fail', 'msg': "로그아웃 상태!"})
-        except:
-            return jsonify({'result': 'fail', 'msg': "로그아웃 상태!"})
+        return jsonify({'result': 'fail'})
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+
+@app.route('/card')
+def card():
+    if 'user' in session:
+        # 로그인 상태가 아닐 경우 (세션에 user가 없을 경우)
+        return render_template('cards.html')
+    return redirect(url_for('home'))
+
+
+@app.route('/showAll', methods=['GET'])
+def show_gourmet_all():
+    result = list(db.gourmet.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'gourmet': result})
+
+
+@app.route('/showSelected', methods=['POST'])
+def show_gourmet_selected():
+    menu_receive = request.form['menu_give']
+    result = list(db.gourmet.find({'menu':menu_receive}, {'_id': 0}))
+    print(result)
+    return jsonify({'result': 'success', 'gourmet': result})
+
+
+@app.route('/post/like', methods=['POST'])
+def like_gourmet():
+    name_receive = request.form['name_give']
+
+    gourmet = db.gourmet.find_one({'name': name_receive})
+    new_like = gourmet['like'] + 1
+    db.gourmet.update_one({'name': name_receive}, {'$set': {'like': new_like}})
+    return jsonify({'result': 'success'})
+
+
+@app.route('/post/delete', methods=['POST'])
+def delete_gourmet():
+    name_receive = request.form['name_give']
+
+    db.gourmet.delete_one({'name': name_receive})
+    return jsonify({'result': 'success'})
+
+
+@app.route('/post', methods=['POST'])
+def post_gourmet():
+    # 1. 클라이언트로부터 데이터를 받기
+    name_receive = request.form['name_give']  # 클라이언트로부터 이름을 받는 부분
+    url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
+    menu_receive = request.form['menu_give']  # 클라이언트로부터 url을 받는 부분
+
+    # 3. mongoDB에 데이터를 넣기
+
+    # 타겟 URL을 읽어서 HTML를 받아오고,
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    # name = soup.select_one('#og\:title')
+    imgurl = soup.select_one('#og\:image')['content']
+    # tmp = soup.select_one('place_thumb')
+    # imgurl = tmp.find('img')
+
+    # print(tmp)  # HTML을 받아온 것을 확인할 수 있다.
+
+    gourmet = {
+        'name': name_receive,
+        'imgurl': imgurl,
+        'menu': menu_receive,
+        'like': 0
+    }
+
+    db.gourmet.insert_one(gourmet)
+
+    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
